4a28fa64e55577a840379b1ecbf12c5a28a66132
meng <3045117496@qq.com>
2024-05-30 11:36:22 +0800

    third commit

--- src/main/java/com/example/gitdemo1/git/GitTest.java
+++ src/main/java/com/example/gitdemo1/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git !!!");
         System.out.print("hello meng !!!");
+        System.out.print("hello meng !!!");
     }
 }

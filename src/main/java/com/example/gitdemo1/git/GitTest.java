d6c53751eb0096b6134245e11edf759538fa7bb5
meng <3045117496@qq.com>
2024-05-30 11:35:00 +0800

    second commit

--- src/main/java/com/example/gitdemo1/git/GitTest.java
+++ src/main/java/com/example/gitdemo1/git/GitTest.java
@@ -3,5 +3,6 @@ package com.example.gitdemo1.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git !!!");
+        System.out.print("hello meng !!!");
     }
 }
